4826e826173dd1d69a49c0a1e9c2cb91581521af
rahuldivyansh <90146156+rahuldivyansh@users.noreply.github.com>
2023-07-23 15:21:29 +0530

    update

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -98,44 +98,6 @@ const Work = () => {
             </div>
           </div>
 
-          {/* Card Item   */}
-          <div
-            style={{
-              backgroundImage: `url(${restaurantImg})`,
-              backgroundSize: "contain",
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "center",
-              width: "100%",
-            }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover Effects  */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Restaurant App
-              </span>
-              <div className="pt-8 text-center">
-                <a
-                  href="https://rahuldivyansh-restaurant-app.netlify.app/"
-                  target="_blank"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white font-bold text-gray-700 text-lg">
-                    
-                    Demo
-                  </button>
-                </a>
-                <a
-                  href="https://github.com/rahuldivyansh/complete_react2021_thapa_technical"
-                  target="_blank"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white font-bold text-gray-700 text-lg">
-                    
-                    Source Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
 
           {/* Card Item   */}
           <div
